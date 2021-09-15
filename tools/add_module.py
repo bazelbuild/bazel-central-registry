9d4e9216de3a6b3a54b4a778dc0c9db25c7c929d
Yun Peng <pcloudy@google.com>
2021-09-15 16:13:26 +0200

    Add tools for bazel central registry

---
+++ tools/add_module.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+"""An interative script to add modules into a Bazel registry.
+
+What this script can do:
+  - Initialize the Bazel Registry.
+  - Generate/update the metadata.json for a module.
+  - Add specified MODULE.bazel
+  - Generate MODULE.bazel file with given module information
+    - module name
+    - version
+    - compatiblity level
+    - dependencies
+  - Generate the source.json file with given source information
+    - The archive url
+    - patch files
+  - Add specified BUILD file for non-Bazel project by turning it into a patch.
+  - Add specified presubmit.yml file.
+  - Generate presubmit.yml file by given build & test targets.
+
+"""
+
+import argparse
+import sys
+import time
+
+from colorama import Fore, Style
+from registry import Module
+from registry import RegistryClient
+from registry import log
+
+
+def yes_or_no(question, default):
+  if default:
+    question += " [Y/n]: "
+  else:
+    question += " [y/N]: "
+
+  var = None
+  while var is None:
+    user_input = ask_input(question).strip().lower()
+    if user_input == "y":
+      var = True
+    elif user_input == "n":
+      var = False
+    elif not user_input:
+      var = default
+    else:
+      print("Invalid selection: {}".format(user_input))
+  return var
+
+
+def ask_input(msg):
+  return input(f"{Fore.YELLOW}ACTION: {Style.RESET_ALL}{msg}")
+
+
+def from_user_input():
+  name = ask_input("Please enter the module name: ")
+  version = ask_input("Please enter the module version: ")
+  compatibility = ask_input(
+      "Please enter the compatibility level [default is 1]: ") or "1"
+  module = Module(name, version, compatibility)
+
+  url = ask_input("Please enter the URL of the source archive: ")
+  strip_prefix = ask_input(
+      "Please enter the strip_prefix value of the archive [default None]: ") or None
+  module.set_source(url, strip_prefix)
+
+  if yes_or_no("Do you want to add patch files?", False):
+    patches = ask_input("Please enter patch file paths, separated by `,`: ")
+    for patch in patches.strip().split(","):
+      module.add_patch(patch.strip())
+    patch_strip = ask_input("Please enter the patch strip number [Default is 1, compatible with git generated "
+                            "patches]: ") or 1
+    module.set_patch_strip(int(patch_strip.strip()))
+
+  if yes_or_no("Do you want to add a BUILD file?", False):
+    build_file = ask_input(
+        "Please enter the path of the BUILD file to be added: ")
+    module.set_build_file(build_file.strip())
+
+  if yes_or_no("Do you want to specify a MODULE.bazel file?", False):
+    path = ask_input("Please enter the MODULE.bazel file path: ").strip()
+    module.set_module_dot_bazel(path)
+  else:
+    if yes_or_no("Do you want to specify dependencies for this module?", False):
+      deps = ask_input(
+          "Please enter dependencies in the form of <name>:<version>, separated by `,`: ")
+      for dep in deps.strip().split(","):
+        name, version = dep.split(":")
+        module.add_dep(name, version)
+
+  if yes_or_no("Do you want to specify a presubmit.yml file", False):
+    path = ask_input("Please enter the presubmit.yml file path: ").strip()
+    module.set_presubmit_yml(path)
+  else:
+    first = True
+    while not (module.build_targets or module.test_targets):
+      if not first:
+        print("Build targets and test targets cannot both be empty, please re-enter!")
+      first = False
+      build_targets = ask_input(
+          "Please enter a list of build targets for this module, separated by `,`: ")
+      for target in build_targets.strip().split(","):
+        if target:
+          module.add_build_target(target)
+      test_targets = ask_input(
+          "Please enter a list of test targets for this module, separated by `,`: ")
+      for target in test_targets.strip().split(","):
+        if target:
+          module.add_test_targets(target)
+  return module
+
+
+def get_maintainers_from_input():
+  maintainers = []
+  while True:
+    maintainer = {}
+    name = ask_input(
+        "Please enter maintainer name [default: The Bazel Team]: ") or "The Bazel Team"
+    maintainer["name"] = name
+    email = ask_input(
+        "Please enter the maintainer's email address [default: bazel-dev@googlegroups.com]: ") \
+        or "bazel-dev@googlegroups.com"
+    maintainer["email"] = email
+    username = ask_input(
+        "(Optional) Please enter the maintainer's github username: ")
+    if username:
+      maintainer["github"] = username
+    maintainers.append(maintainer)
+    if not yes_or_no("Do you want to add another maintainer?", False):
+      break
+  return maintainers
+
+
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv[1:]
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--registry", type=str, default=".",
+                      help="Specifiy the root path of the registry (default: the current working directory).")
+  parser.add_argument(
+      "--input", type=str, help="Take module information from a json file, which can be generated from previous input.")
+
+  args = parser.parse_args(argv)
+
+  if args.input:
+    log(f"Getting module information from {args.input}...")
+    module = Module()
+    module.from_json(args.input)
+  else:
+    log("Getting module information from user input...")
+    module = from_user_input()
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    log(f"Saving module information to {module.name}.{timestamp}.json")
+    log(f"You can use it via --input={module.name}.{timestamp}.json")
+    module.dump(f"{module.name}.{timestamp}.json")
+
+  client = RegistryClient(args.registry)
+
+  if not client.contains(module.name):
+    log(f"{module.name} is a new Bazel module...")
+    homepage = ask_input(
+        "Please enter the homepage url for this module: ").strip()
+    maintainers = get_maintainers_from_input()
+    client.init_module(module.name, maintainers, homepage)
+
+  client.add(module, override=True)
+  log(f"{module.name} {module.version} is added into the registry.")
+
+
+if __name__ == "__main__":
+  sys.exit(main())
---
+++ tools/registry.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python3
+#
+# Copyright 2021 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# pylint: disable=missing-function-docstring
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=unspecified-encoding
+# pylint: disable=invalid-name
+"""Tool classes to handle a Bazel registry"""
+
+import base64
+import difflib
+import hashlib
+import json
+import pathlib
+import shutil
+import urllib.request
+import yaml
+
+from colorama import Fore, Style
+
+
+def log(msg):
+  print(f"{Fore.GREEN}INFO: {Style.RESET_ALL}{msg}")
+
+
+def download(url):
+  with urllib.request.urlopen(url) as response:
+    return response.read()
+
+
+def read(path):
+  with open(path, "rb") as file:
+    return file.read()
+
+
+def integrity(data):
+  hash_value = hashlib.sha256(data)
+  return "sha256-" + base64.b64encode(hash_value.digest()).decode()
+
+
+def json_dump(file, data):
+  with open(file, "w") as f:
+    json.dump(data, f, indent=4, sort_keys=True)
+
+
+class Module:
+  """A class to represent all information of a Bazel module."""
+
+  def __init__(self, name=None, version=None, compatibility_level=1):
+    self.name = name
+    self.version = version
+    self.compatibility_level = compatibility_level
+    self.module_dot_bazel = None
+    self.url = None
+    self.strip_prefix = None
+    self.deps = []
+    self.patches = []
+    self.patch_strip = 0
+    self.build_file = None
+    self.presubmit_yml = None
+    self.build_targets = []
+    self.test_targets = []
+
+  def add_dep(self, module_name, version):
+    self.deps.append((module_name, version))
+    return self
+
+  def set_module_dot_bazel(self, module_dot_bazel):
+    self.module_dot_bazel = module_dot_bazel
+    return self
+
+  def set_source(self, url, strip_prefix=None):
+    self.url = url
+    self.strip_prefix = strip_prefix
+    return self
+
+  def add_patch(self, patch_file):
+    self.patches.append(patch_file)
+    return self
+
+  def set_patch_strip(self, patch_strip):
+    self.patch_strip = patch_strip
+    return self
+
+  def set_build_file(self, build_file):
+    self.build_file = build_file
+    return self
+
+  def set_presubmit_yml(self, presubmit_yml):
+    self.presubmit_yml = presubmit_yml
+    return self
+
+  def add_build_target(self, target):
+    if not target.startswith("@" + self.name):
+      target = "@" + self.name + target
+    self.build_targets.append(target)
+    return self
+
+  def add_test_targets(self, target):
+    if not target.startswith("@" + self.name):
+      target = "@" + self.name + target
+    self.test_targets.append(target)
+    return self
+
+  def dump(self, file):
+    json_dump(file, self.__dict__)
+
+  def from_json(self, file):
+    with open(file) as f:
+      self.__dict__ = json.load(f)
+
+
+class RegistryException(Exception):
+  """
+  Raised whenever something goes wrong with modifying the registry.
+  """
+
+
+class RegistryClient:
+  """A class to help create a Bazel registry."""
+
+  _MODULE_BAZEL = """
+module(
+    name = "{0}",
+    version = "{1}",
+    compatibility_level = {2},
+)
+""".strip()
+
+  def __init__(self, root):
+    self.root = pathlib.Path(root)
+
+  def contains(self, module_name, version=None):
+    """
+    Check if the registry contains a module or a specific version of a
+    module by verifying if the directory exists.
+    """
+    dir_path = self.root.joinpath("modules", module_name)
+    if version:
+      dir_path = dir_path.joinpath(version)
+    return dir_path.is_dir()
+
+  def init_module(self, module_name, maintainers, homepage):
+    """
+    Initialize a module, create the directory and metadata.json file.
+
+    Parameters
+    ----------
+    module_name : str
+        The module name
+    maintainers : list of maps of string -> string
+        The maintainer information, eg
+         [{"name": "John", "email": "john@guugoo.com"},
+          {"name": "Yun", "github": "meteorcloudy"}]
+    homepage : str
+        A URL to the project's homepage
+
+    """
+    p = self.root.joinpath("modules", module_name)
+    p.mkdir(parents=True, exist_ok=True)
+
+    # Create metadata.json file
+    metadata = {
+        "maintainers": maintainers,
+        "homepage": homepage,
+        "versions": [],
+        "yanked_versions": {},
+    }
+    json_dump(p.joinpath("metadata.json"), metadata)
+
+  def add(self, module, override=False):
+    """
+    Add a new module version, the module must be already initialized
+
+    Parameters
+    ----------
+    module : Module
+        A Module instance containing information of the module version to
+        be added
+    override : Whether to override existing module
+    """
+    # Check if the module version already exists
+    if self.contains(module.name, module.version):
+      if override:
+        log("Overridding module '%s' at version '%s'..." %
+            (module.name, module.version))
+        self.delete(module.name, module.version)
+      else:
+        raise RegistryException(
+            f"Version {module.version} for module {module.name} already exists.")
+
+    p = self.root.joinpath("modules", module.name, module.version)
+    p.mkdir()
+
+    # Create MODULE.bazel
+    module_dot_bazel = p.joinpath("MODULE.bazel")
+    if module.module_dot_bazel:
+      shutil.copy(module.module_dot_bazel, module_dot_bazel)
+    else:
+      deps = "\n".join(
+          f"bazel_dep(name = \"{name}\", version = \"{version}\")"
+          for name, version in module.deps)
+      with module_dot_bazel.open("w") as f:
+        f.write(self._MODULE_BAZEL.format(
+            module.name, module.version,
+            module.compatibility_level) + deps)
+        f.write("\n")
+
+    # Create source.json & copy patch files to the registry
+    source = {
+        "url": module.url,
+        "integrity": integrity(download(module.url)),
+    }
+    if module.strip_prefix:
+      source["strip_prefix"] = module.strip_prefix
+
+    patch_dir = p.joinpath("patches")
+    if module.patches or module.build_file:
+      patch_dir.mkdir()
+      source["patches"] = {}
+      source["patch_strip"] = module.patch_strip
+
+    if module.patches:
+      for s in module.patches:
+        patch = pathlib.Path(s)
+        source["patches"][patch.name] = integrity(read(patch))
+        shutil.copy(patch, patch_dir)
+
+    # Turn additional BUILD file into a patch
+    if module.build_file:
+      build_file_content = pathlib.Path(module.build_file).open().readlines()
+      build_file = "a/" * module.patch_strip + "BUILD.bazel"
+      patch_content = difflib.unified_diff(
+          [], build_file_content, "/dev/null", build_file)
+      patch_name = "add_build_file.patch"
+      patch = patch_dir.joinpath(patch_name)
+      with patch.open("w") as f:
+        f.writelines(patch_content)
+      source["patches"][patch_name] = integrity(read(patch))
+
+    json_dump(p.joinpath("source.json"), source)
+
+    # Create presubmit.yml file
+    presubmit_yml = p.joinpath("presubmit.yml")
+    if module.presubmit_yml:
+      shutil.copy(module.presubmit_yml, presubmit_yml)
+    else:
+      platforms = {
+          "centos7": {},
+          "debian10": {},
+          "ubuntu2004": {},
+          "macos": {},
+          "windows": {},
+      }
+      for _, config in platforms.items():
+        if module.build_targets:
+          config["build_targets"] = module.build_targets.copy()
+        if module.test_targets:
+          config["test_targets"] = module.test_targets.copy()
+      with presubmit_yml.open("w") as f:
+        yaml.dump({"platforms": platforms}, f)
+
+    # Add new version to metadata.json
+    metadata_path = self.root.joinpath("modules", module.name,
+                                       "metadata.json")
+    metadata = json.load(metadata_path.open())
+    metadata["versions"].append(module.version)
+    metadata["versions"] = list(set(metadata["versions"])).sort()
+    json_dump(metadata_path, metadata)
+
+  def delete(self, module_name, version):
+    """Delete an existing module version."""
+    p = self.root.joinpath("modules", module_name)
+    shutil.rmtree(p.joinpath(version))
+    metadata_path = p.joinpath("metadata.json")
+    metadata = json.load(metadata_path.open())
+    if version in metadata["versions"]:
+      metadata["versions"].remove(version)
+    json_dump(metadata_path, metadata)
