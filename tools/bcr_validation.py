06316637a991be0c4bc1ef19fe55c4bae0e9d98d
Yun Peng <pcloudy@google.com>
2022-10-21 18:22:18 +0200

    Add BCR validation script (#229)
    * Add sanity check script
    
    * Add proper version comparison
    
    * Some improvements
    
    * small fix
    
    * small fix
    
    * Address review comment
    
    * Address more comments
    
    * Check url path after resolve it
    
    * Rename to bcr_check.py
    
    * Rename check to validation

---
+++ tools/bcr_validation.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+"""A script to perform BCR validations for Bazel modules
+
+Validations performed are:
+  - Verify versions in metadata.json matches existing versions
+  - Verify the source archive URL match the source repositories
+  - Verify the source archive URL is stable
+  - Verify if the presubmit.yml file matches the previous version
+    - If not, we should require BCR maintainer review.
+"""
+
+import argparse
+import sys
+import os
+
+from enum import Enum
+from difflib import unified_diff
+from urllib.parse import urlparse
+
+from registry import RegistryClient
+from registry import Version
+from verify_stable_archives import UrlStability
+from verify_stable_archives import verify_stable_archive
+
+
+class BcrValidationResult(Enum):
+  ALL_GOOD = 1
+  NEED_BCR_MAINTAINER_REVIEW = 2
+  FAILED = 3
+
+RED = "\x1b[31m"
+GREEN = "\x1b[32m"
+YELLOW = "\x1b[33m"
+RESET = "\x1b[0m"
+
+COLOR = {
+  BcrValidationResult.ALL_GOOD: GREEN,
+  BcrValidationResult.NEED_BCR_MAINTAINER_REVIEW: YELLOW,
+  BcrValidationResult.FAILED: RED,
+}
+
+def print_collapsed_group(name):
+    print("\n\n--- {0}\n\n".format(name))
+
+def print_expanded_group(name):
+    print("\n\n+++ {0}\n\n".format(name))
+
+def parse_module_versions(registry, check_all, inputs):
+  """Parse module versions to be validated from input."""
+  if check_all:
+    return registry.get_all_module_versions()
+  result = []
+  for s in inputs:
+    if "@" in s:
+      name, version = s.split("@")
+      result.append((name, version))
+    else:
+      result.extend(registry.get_module_versions(s))
+  return result
+
+def print_validation_result(result):
+  for code, message in result:
+    color = COLOR[code]
+    print(f"{color}{code}{RESET}: {message}\n")
+
+def validate_module(registry, module_name, version):
+  print_collapsed_group(f"Validating {module_name}@{version}")
+
+  validation_results = []
+
+  # Verify the version is recorded in metadata.json and the directory entry exists.
+  versions = registry.get_metadata(module_name)["versions"]
+  if version not in versions:
+    validation_results.append((BcrValidationResult.FAILED, f"Version {version} is not recorded in {module_name}'s metadata.json file."))
+  if not registry.contains(module_name, version):
+    validation_results.append((BcrValidationResult.FAILED, f"{module_name}@{version} doesn't exist."))
+  if validation_results:
+    print_validation_result(validation_results)
+    return validation_results
+
+  # Verify the source archive URL matches the github repo. For now, we only support github repositories check.
+  source_url = registry.get_source(module_name, version)["url"]
+  source_repositories = registry.get_metadata(module_name).get("repository", [])
+  matched = False
+  for source_repository in source_repositories:
+    if matched:
+      break
+    repo_type, repo_path = source_repository.split(":")
+    if repo_type == "github":
+      parts = urlparse(source_url)
+      matched = parts.scheme == "https" and parts.netloc == "github.com" and os.path.abspath(parts.path).startswith(f"/{repo_path}/")
+  if not matched:
+    validation_results.append((BcrValidationResult.FAILED, f"The source URL of {module_name}@{version} ({source_url}) doesn't match any of the module's source repositories {source_repositories}."))
+
+  # Verify source archive URL is stable
+  if verify_stable_archive(source_url) == UrlStability.UNSTABLE:
+      validation_results.append((BcrValidationResult.FAILED,
+                          f"{module_name}@{version} is using an unstable source url: `{source_url}`.\n"
+                          + "The source url should follow the format of `https://github.com/<ORGANIZATION>/<REPO>/archive/refs/tags/<TAG>.tar.gz` to retrieve a source archive that is guaranteed by GitHub to be stable over time.\n"
+                          + "See https://github.com/bazel-contrib/SIG-rules-authors/issues/11#issuecomment-1029861300 for more context."))
+
+  # Verify if the presubmit.yml is the same as the previous version.
+  versions.sort(key=Version)
+  index = versions.index(version)
+  if index == 0:
+    validation_results.append((BcrValidationResult.NEED_BCR_MAINTAINER_REVIEW, f"Module version {module_name}@{version} is new, the presubmit.yml file should be reviewed by a BCR maintainer."))
+  elif index > 0:
+    pre_version = versions[index - 1]
+    previous_presubmit_yml = registry.get_presubmit_yml_path(module_name, pre_version)
+    previous_presubmit_content = open(previous_presubmit_yml, "r").readlines()
+    current_presubmit_yml = registry.get_presubmit_yml_path(module_name, version)
+    current_presubmit_content = open(current_presubmit_yml, "r").readlines()
+    diff = list(unified_diff(previous_presubmit_content, current_presubmit_content, fromfile=str(previous_presubmit_yml), tofile = str(current_presubmit_yml)))
+    if diff:
+      validation_results.append((BcrValidationResult.NEED_BCR_MAINTAINER_REVIEW,
+                          f"The presubmit.yml file of {module_name}@{version} doesn't match its previous version {module_name}@{pre_version}, the following presubmit.yml file change should be reviewed by a BCR maintainer.\n"
+                          + "".join(diff)))
+
+  if not validation_results:
+    validation_results.append((BcrValidationResult.ALL_GOOD, "Everything looks good!"))
+
+  print_validation_result(validation_results)
+  return validation_results
+
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv[1:]
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    "--registry",
+    type=str,
+    default=".",
+    help="Specify the root path of the registry (default: the current working directory).")
+  parser.add_argument(
+    "--check",
+    type=str,
+    action = "append",
+    help="Specify a Bazel module version you want to perform the BCR check on."
+    + " (e.g. bazel_skylib@1.3.0). If no version is specified, all versions of that module are checked."
+    + " This flag can be repeated to accept multiple module versions.")
+  parser.add_argument(
+    "--check_all",
+    action="store_true",
+    help="Check all Bazel modules in the registry, ignore other --check flags.")
+
+  args = parser.parse_args(argv)
+
+  if not args.check_all and not args.check:
+    parser.print_help()
+    return -1
+
+  registry = RegistryClient(args.registry)
+
+  # Parse what module versions we should validate
+  module_versions = parse_module_versions(registry, args.check_all, args.check)
+  print_collapsed_group("Module versions to be validated:")
+  for name, version in module_versions:
+    print(f"{name}@{version}")
+
+  # Validate given module version.
+  validation_results = []
+  for name, version in module_versions:
+    validation_results.extend(validate_module(registry, name, version))
+
+  # Calculate the return code
+  # 0: All good
+  # 1: BCR validation failed
+  # 42: BCR validation passes, but some changes need BCR maintainer review before trigging follow up BCR presubmit jobs.
+  result_codes = [code for code, _ in validation_results]
+  if BcrValidationResult.FAILED in result_codes:
+    return 1
+  if BcrValidationResult.NEED_BCR_MAINTAINER_REVIEW in result_codes:
+    # Use a special return code to avoid conflict with other error code
+    return 42
+  return 0
+
+if __name__ == "__main__":
+  sys.exit(main())
--- tools/registry.py
+++ tools/registry.py
@@ -21,10 +21,12 @@
 
 import base64
 import difflib
+import functools
 import hashlib
 import json
 import netrc
 import pathlib
+import re
 import shutil
 import urllib.parse
 import urllib.request
@@ -70,6 +72,54 @@ def json_dump(file, data):
   with open(file, "w") as f:
     json.dump(data, f, indent=4, sort_keys=True)
     f.write("\n")
+
+# Translated from:
+# https://github.com/bazelbuild/bazel/blob/79a53def2ebbd9358450f739ea37bf70662e8614/src/main/java/com/google/devtools/build/lib/bazel/bzlmod/Version.java#L58
+@functools.total_ordering
+class Version:
+
+  @functools.total_ordering
+  class Identifier:
+    def __init__(self, s):
+      if not s:
+        raise RegistryException("identifier is empty")
+      self.val = int(s) if s.isnumeric() else s
+
+    def __eq__(self, other):
+      if type(self.val) != type(other.val):
+        return False
+      return self.val == other.val
+
+    def __lt__(self, other):
+      if type(self.val) != type(other.val):
+        return type(self.val) == int
+      return self.val < other.val
+
+  @staticmethod
+  def convert_to_identifiers(s):
+    if s == None:
+      return None
+    return [Version.Identifier(i) for i in s.split(".")]
+
+  def __init__(self, version_str):
+    PATTERN = re.compile(r"^([a-zA-Z0-9.]+)(?:-([a-zA-Z0-9.-]+))?(?:\+[a-zA-Z0-9.-]+)?$")
+    m = PATTERN.match(version_str)
+    if not m:
+      raise RegistryException(f"`{version_str}` is not a valid version")
+    self.release = Version.convert_to_identifiers(m.groups()[0])
+    self.prerelease = Version.convert_to_identifiers(m.groups()[1])
+
+  def __eq__(self, other):
+    return (self.release, self.prerelease) == (other.release, other.prerelease)
+
+  def __lt__(self, other):
+    if self.release != other.release:
+      return self.release < other.release
+    if self.prerelease == None:
+      return False
+    if other.prerelease == None:
+      return True
+    return self.prerelease < other.prerelease
 
 
 class Module:
@@ -167,14 +217,17 @@ module(
     modules_dir = self.root.joinpath("modules")
     return [path.name for path in modules_dir.iterdir()]
 
+  def get_module_versions(self, module_name):
+    module_versions = []
+    metadata = self.get_metadata(module_name)
+    for version in metadata["versions"]:
+      module_versions.append((module_name, version))
+    return module_versions
+
   def get_all_module_versions(self):
     module_versions = []
-
     for module_name in self.get_all_modules():
-      metadata = self.get_metadata(module_name)
-      for version in metadata["versions"]:
-        module_versions.append((module_name, version))
-
+      module_versions.extend(self.get_module_versions(module_name))
     return module_versions
 
   def get_metadata(self, module_name):
@@ -186,6 +239,10 @@ module(
     source_path = self.root.joinpath("modules", module_name, version,
                                      "source.json")
     return json.load(source_path.open())
+
+  def get_presubmit_yml_path(self, module_name, version):
+    return self.root.joinpath("modules", module_name, version,
+                              "presubmit.yml")
 
   def contains(self, module_name, version=None):
     """
@@ -351,8 +408,7 @@ module(
     metadata = json.load(metadata_path.open())
     metadata["versions"].append(module.version)
     metadata["versions"] = list(set(metadata["versions"]))
-    # TODO(pcloudy): versions should be sorted with the same logic in Bzlmod.
-    metadata["versions"].sort()
+    metadata["versions"].sort(key=Version)
     json_dump(metadata_path, metadata)
 
   def delete(self, module_name, version):
