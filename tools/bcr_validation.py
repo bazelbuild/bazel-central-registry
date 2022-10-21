8579f8b24af09d18a0f30a58c0b033fdaf330e3a
Yun Peng <pcloudy@google.com>
2022-10-21 18:15:21 +0200

    Rename check to validation

--- tools/bcr_validation.py
+++ tools/bcr_validation.py
@@ -16,9 +16,9 @@
 # pylint: disable=invalid-name
 # pylint: disable=line-too-long
 # pylint: disable=missing-function-docstring
-"""A script to perform BCR checks for Bazel modules
+"""A script to perform BCR validations for Bazel modules
 
-BCR checks performed are:
+Validations performed are:
   - Verify versions in metadata.json matches existing versions
   - Verify the source archive URL match the source repositories
   - Verify the source archive URL is stable
@@ -40,7 +40,7 @@ from verify_stable_archives import UrlStability
 from verify_stable_archives import verify_stable_archive
 
 
-class BcrCheckResult(Enum):
+class BcrValidationResult(Enum):
   ALL_GOOD = 1
   NEED_BCR_MAINTAINER_REVIEW = 2
   FAILED = 3
@@ -51,9 +51,9 @@ YELLOW = "\x1b[33m"
 RESET = "\x1b[0m"
 
 COLOR = {
-  BcrCheckResult.ALL_GOOD: GREEN,
-  BcrCheckResult.NEED_BCR_MAINTAINER_REVIEW: YELLOW,
-  BcrCheckResult.FAILED: RED,
+  BcrValidationResult.ALL_GOOD: GREEN,
+  BcrValidationResult.NEED_BCR_MAINTAINER_REVIEW: YELLOW,
+  BcrValidationResult.FAILED: RED,
 }
 
 def print_collapsed_group(name):
@@ -63,7 +63,7 @@ def print_expanded_group(name):
     print("\n\n+++ {0}\n\n".format(name))
 
 def parse_module_versions(registry, check_all, inputs):
-  """Parse module versions to be checked from input."""
+  """Parse module versions to be validated from input."""
   if check_all:
     return registry.get_all_module_versions()
   result = []
@@ -75,25 +75,25 @@ def parse_module_versions(registry, check_all, inputs):
       result.extend(registry.get_module_versions(s))
   return result
 
-def print_check_result(result):
+def print_validation_result(result):
   for code, message in result:
     color = COLOR[code]
     print(f"{color}{code}{RESET}: {message}\n")
 
-def BCR_check(registry, module_name, version):
-  print_collapsed_group(f"Checking {module_name}@{version}")
+def validate_module(registry, module_name, version):
+  print_collapsed_group(f"Validating {module_name}@{version}")
 
-  check_results = []
+  validation_results = []
 
-  # Check the version is recorded in metadata.json and the directory entry exists.
+  # Verify the version is recorded in metadata.json and the directory entry exists.
   versions = registry.get_metadata(module_name)["versions"]
   if version not in versions:
-    check_results.append((BcrCheckResult.FAILED, f"Version {version} is not recorded in {module_name}'s metadata.json file."))
+    validation_results.append((BcrValidationResult.FAILED, f"Version {version} is not recorded in {module_name}'s metadata.json file."))
   if not registry.contains(module_name, version):
-    check_results.append((BcrCheckResult.FAILED, f"{module_name}@{version} doesn't exist."))
-  if check_results:
-    print_check_result(check_results)
-    return check_results
+    validation_results.append((BcrValidationResult.FAILED, f"{module_name}@{version} doesn't exist."))
+  if validation_results:
+    print_validation_result(validation_results)
+    return validation_results
 
   # Verify the source archive URL matches the github repo. For now, we only support github repositories check.
   source_url = registry.get_source(module_name, version)["url"]
@@ -107,20 +107,20 @@ def BCR_check(registry, module_name, version):
       parts = urlparse(source_url)
       matched = parts.scheme == "https" and parts.netloc == "github.com" and os.path.abspath(parts.path).startswith(f"/{repo_path}/")
   if not matched:
-    check_results.append((BcrCheckResult.FAILED, f"The source URL of {module_name}@{version} ({source_url}) doesn't match any of the module's source repositories {source_repositories}."))
+    validation_results.append((BcrValidationResult.FAILED, f"The source URL of {module_name}@{version} ({source_url}) doesn't match any of the module's source repositories {source_repositories}."))
 
-  # Check source archive URL is stable
+  # Verify source archive URL is stable
   if verify_stable_archive(source_url) == UrlStability.UNSTABLE:
-      check_results.append((BcrCheckResult.FAILED,
+      validation_results.append((BcrValidationResult.FAILED,
                           f"{module_name}@{version} is using an unstable source url: `{source_url}`.\n"
                           + "The source url should follow the format of `https://github.com/<ORGANIZATION>/<REPO>/archive/refs/tags/<TAG>.tar.gz` to retrieve a source archive that is guaranteed by GitHub to be stable over time.\n"
                           + "See https://github.com/bazel-contrib/SIG-rules-authors/issues/11#issuecomment-1029861300 for more context."))
 
-  # Check if the presubmit.yml is the same as the previous version.
+  # Verify if the presubmit.yml is the same as the previous version.
   versions.sort(key=Version)
   index = versions.index(version)
   if index == 0:
-    check_results.append((BcrCheckResult.NEED_BCR_MAINTAINER_REVIEW, f"Module version {module_name}@{version} is new, the presubmit.yml file should be reviewed by a BCR maintainer."))
+    validation_results.append((BcrValidationResult.NEED_BCR_MAINTAINER_REVIEW, f"Module version {module_name}@{version} is new, the presubmit.yml file should be reviewed by a BCR maintainer."))
   elif index > 0:
     pre_version = versions[index - 1]
     previous_presubmit_yml = registry.get_presubmit_yml_path(module_name, pre_version)
@@ -129,15 +129,15 @@ def BCR_check(registry, module_name, version):
     current_presubmit_content = open(current_presubmit_yml, "r").readlines()
     diff = list(unified_diff(previous_presubmit_content, current_presubmit_content, fromfile=str(previous_presubmit_yml), tofile = str(current_presubmit_yml)))
     if diff:
-      check_results.append((BcrCheckResult.NEED_BCR_MAINTAINER_REVIEW,
+      validation_results.append((BcrValidationResult.NEED_BCR_MAINTAINER_REVIEW,
                           f"The presubmit.yml file of {module_name}@{version} doesn't match its previous version {module_name}@{pre_version}, the following presubmit.yml file change should be reviewed by a BCR maintainer.\n"
                           + "".join(diff)))
 
-  if not check_results:
-    check_results.append((BcrCheckResult.ALL_GOOD, "Everything looks good!"))
+  if not validation_results:
+    validation_results.append((BcrValidationResult.ALL_GOOD, "Everything looks good!"))
 
-  print_check_result(check_results)
-  return check_results
+  print_validation_result(validation_results)
+  return validation_results
 
 def main(argv=None):
   if argv is None:
@@ -169,25 +169,25 @@ def main(argv=None):
 
   registry = RegistryClient(args.registry)
 
-  # Parse what module versions we should check
+  # Parse what module versions we should validate
   module_versions = parse_module_versions(registry, args.check_all, args.check)
-  print_collapsed_group("Module versions to be checked:")
+  print_collapsed_group("Module versions to be validated:")
   for name, version in module_versions:
     print(f"{name}@{version}")
 
-  # Perform checks on given module version.
-  check_results = []
+  # Validate given module version.
+  validation_results = []
   for name, version in module_versions:
-    check_results.extend(BCR_check(registry, name, version))
+    validation_results.extend(validate_module(registry, name, version))
 
   # Calculate the return code
   # 0: All good
-  # 1: BCR check failed
-  # 42: BCR check pass, but some changes need BCR maintainer review before trigging follow up BCR presubmit jobs.
-  result_codes = [code for code, _ in check_results]
-  if BcrCheckResult.FAILED in result_codes:
+  # 1: BCR validation failed
+  # 42: BCR validation passes, but some changes need BCR maintainer review before trigging follow up BCR presubmit jobs.
+  result_codes = [code for code, _ in validation_results]
+  if BcrValidationResult.FAILED in result_codes:
     return 1
-  if BcrCheckResult.NEED_BCR_MAINTAINER_REVIEW in result_codes:
+  if BcrValidationResult.NEED_BCR_MAINTAINER_REVIEW in result_codes:
     # Use a special return code to avoid conflict with other error code
     return 42
   return 0
