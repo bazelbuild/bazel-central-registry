01ecda9741042820688d290d8ee3b4e093b905fd
Yun Peng <pcloudy@google.com>
2022-12-05 12:21:43 +0100

    Add doc about depending on the BCR infra (#303)
    * Add doc about depending on the BCR infra
    
    * Polish the wording
    
    * More fixes
    
    * Add tools/print_all_src_urls.py

---
+++ tools/print_all_src_urls.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#
+# Copyright 2022 The Bazel Authors. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# pylint: disable=invalid-name
+# pylint: disable=line-too-long
+# pylint: disable=missing-function-docstring
+
+import sys
+
+from registry import RegistryClient
+
+def main():
+  client = RegistryClient(".")
+  for name, version in client.get_all_module_versions():
+    print(client.get_source(name, version)["url"])
+
+if __name__ == "__main__":
+  sys.exit(main())
