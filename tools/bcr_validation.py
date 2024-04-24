bazelbuild/bazel-central-registry

commit cb0fb4b2fb3a77ed7c3950bd1485e2e8655374ae

bcr_validation: Validate at least one task is specified (#1862)
This would prevent using outdated presubmit.yml format for BCR modules.

Context:
https://github.com/bazelbuild/bazel-central-registry/pull/1861/files#diff-12d94d573f23b0e6beb29366322c6b4d459839255f9cf227e54051fd28c6e308R1

tools/bcr_validation.py
@@ -250,6 +250,9 @@ class BcrValidator:
     shutil.rmtree(tmp_dir)
 
   def check_if_bazel_version_is_set(self, tasks):
+    if not tasks:
+      self.report(BcrValidationResult.FAILED, "At least one task should be specified in the presubmit.yml file.")
+      return
     for task_name, task_config in tasks.items():
       if "bazel" not in task_config:
         self.report(BcrValidationResult.FAILED, "Missing bazel version for task '%s' in the presubmit.yml file." % task_name)

commit e046ebcd908be31e8b216d4115e5daa80f4f47c2

Reallow modules with no non-test module tasks (#1872)
This regressed in #1862

tools/bcr_validation.py
@@ -250,9 +250,6 @@ class BcrValidator:
     shutil.rmtree(tmp_dir)
 
   def check_if_bazel_version_is_set(self, tasks):
-    if not tasks:
-      self.report(BcrValidationResult.FAILED, "At least one task should be specified in the presubmit.yml file.")
-      return
     for task_name, task_config in tasks.items():
       if "bazel" not in task_config:
         self.report(BcrValidationResult.FAILED, "Missing bazel version for task '%s' in the presubmit.yml file." % task_name)
@@ -261,9 +258,14 @@ class BcrValidator:
     presubmit_yml = self.registry.get_presubmit_yml_path(module_name, version)
     presubmit = yaml.safe_load(open(presubmit_yml, "r"))
     report_num_old = len(self.validation_results)
-    self.check_if_bazel_version_is_set(presubmit.get("tasks", {}))
+    tasks = presubmit.get("tasks", {})
+    self.check_if_bazel_version_is_set(tasks)
+    test_module_tasks = {}
     if "bcr_test_module" in presubmit:
-      self.check_if_bazel_version_is_set(presubmit["bcr_test_module"].get("tasks", {}))
+      test_module_tasks = presubmit["bcr_test_module"].get("tasks", {})
+      self.check_if_bazel_version_is_set(test_module_tasks)
+    if not tasks and not test_module_tasks:
+      self.report(BcrValidationResult.FAILED, "At least one task should be specified in the presubmit.yml file.")
     report_num_new = len(self.validation_results)
     if report_num_new == report_num_old:
       self.report(BcrValidationResult.GOOD, "The presubmit.yml file is valid.")