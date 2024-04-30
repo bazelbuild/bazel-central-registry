4870331fa162ef0b9740ccfd8d81860616d0ffa5
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-04-30 14:45:18 +0200

    Fix `//tools:update_integrity` if there is no `patches` directory

--- tools/registry.py
+++ tools/registry.py
@@ -451,7 +451,10 @@ module(
     source["integrity"] = integrity(download(source["url"]))
     source_path = self.get_source_path(module_name, version)
     patch_dir = source_path.parent / "patches"
-    available = sorted(p.name for p in patch_dir.iterdir())
+    if patch_dir.exists():
+      available = sorted(p.name for p in patch_dir.iterdir())
+    else:
+      available = []
     current = source.get("patches", {}).keys()
     patch_files = [patch_dir / p for p in current if p in available]
     patch_files.extend(patch_dir / p for p in available if p not in current)
