e2529eedab7aa3f7c6de100fa0b3dec2a9a4cc1b
Fabian Meumertzheim <fabian@meumertzhe.im>
2025-06-29 12:46:20 +0200

    Add stats API

--- modules/mimalloc/2.2.4/overlay/test/bcr/api_test.c
+++ modules/mimalloc/2.2.4/overlay/test/bcr/api_test.c
@@ -1,4 +1,5 @@
 #include <mimalloc.h>
+#include <mimalloc-stats.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -21,6 +22,14 @@ int main(int argc, char** argv) {
     printf("Memory leak not detected\n");
     return 1;
   }
+
+  char* json = mi_stats_get_json(0, NULL);
+  if (json == NULL) {
+    printf("Failed to get JSON stats\n");
+    return 1;
+  }
+  puts(json);
+  mi_free(json);
 
   mi_free(p);
 }
