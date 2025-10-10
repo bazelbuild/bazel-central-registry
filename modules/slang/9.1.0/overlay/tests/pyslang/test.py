2b20d4131d45869d2b781e231e86b8aa6621755d
Adam Hizzey <36563979+ahizzey@users.noreply.github.com>
2025-10-10 15:12:32 +0100

    fix python test

--- modules/slang/9.1.0/overlay/tests/pyslang/test.py
+++ modules/slang/9.1.0/overlay/tests/pyslang/test.py
@@ -8,7 +8,8 @@ test_dir = os.path.dirname(__file__)
 def test_pyslang():
     tree = pyslang.SyntaxTree.fromFile(f"{test_dir}/test_module.sv")
     mod = tree.root.members[0]
-    assert mod.header.name.value == "test_module"
+    if mod.header.name.value != "test_module":
+        raise RuntimeError(f"Expected module name 'test_module', got '{mod.header.name.value}'")
 
     return 0
 
