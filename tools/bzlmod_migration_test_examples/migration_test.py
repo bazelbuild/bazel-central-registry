c191ed48369bf51459a9eceb95cb7910dee174ad
Nevena <kotlaja@google.com>
2025-11-20 11:11:17 +0100

    Pypi updates to the bzlmod migration script (#6584)
    This PR involves:
    * Ignore repos starting with `pypi_` from direct deps since they are
    usually not referenced directly. If referenced directly, these repos
    will be fixed in the second part of the script ("parsing the error"
    part).
    * If referenced repo starts with `pypi_<name>`, raise an error since it
    needs to be changed to `pypi//<name>`.
    * Update of a python test to cover these new cases.
    * Update of golden files in the test environment.
    * Smaller updates to the script, usually due to readability.

--- tools/bzlmod_migration_test_examples/migration_test.py
+++ tools/bzlmod_migration_test_examples/migration_test.py
@@ -7,7 +7,7 @@ from unittest import main
 
 class BazelBuildTest(unittest.TestCase):
     """
-    A test suite for verifying Bzlmod migration tool for maven and python extensions.
+    A test suite for verifying Bzlmod migration tool for maven and module extensions.
     """
 
     _CREATED_FILES = [
@@ -17,16 +17,21 @@ class BazelBuildTest(unittest.TestCase):
         "migration_info.md",
         "query_direct_deps",
         "resolved_deps.py",
-        "extension_for_",
     ]
 
     def _cleanup_created_files(self):
         """
         Remove files which were created by migration tool.
         """
+        for file_name in self._CREATED_FILES:
+            file_path = os.path.join(os.getcwd(), file_name)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+
+        # Remove all created extensions.
         my_dir = os.getcwd()
         for fname in os.listdir(my_dir):
-            if any(fname.startswith(p) for p in self._CREATED_FILES):
+            if fname.startswith("extension_for_"):
                 os.remove(os.path.join(my_dir, fname))
 
     def _run_command(self, command):
