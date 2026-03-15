c862bc280134727a45015977e00690f2e800dac6
Finn Ball <finn.ball@codificasolutions.com>
2026-03-15 10:06:53 +0000

    add libvpx@1.16.0.bcr.beta.1

---
+++ modules/libvpx/1.16.0.bcr.beta.1/overlay/config/generic-gnu/vpx_version.h
@@ -0,0 +1,11 @@
+// This file is derived from upstream 1.16.0 version metadata.
+#ifndef VPX_VERSION_H_
+#define VPX_VERSION_H_
+#define VERSION_MAJOR  1
+#define VERSION_MINOR  16
+#define VERSION_PATCH  0
+#define VERSION_EXTRA  ""
+#define VERSION_PACKED ((VERSION_MAJOR<<16)|(VERSION_MINOR<<8)|(VERSION_PATCH))
+#define VERSION_STRING_NOSP "v1.16.0"
+#define VERSION_STRING      " v1.16.0"
+#endif  // VPX_VERSION_H_
