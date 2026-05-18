9303f4a75f81875b5e6f0b768756497e797a0075
Byoungchan Lee <byoungchan.lee@gmx.com>
2026-05-18 21:05:30 +0900

    dawn: trim public Bazel targets

--- modules/dawn/20260410.140140/overlay/tests/webgpu_dawn_consumer_smoke.cc
+++ modules/dawn/20260410.140140/overlay/tests/webgpu_dawn_consumer_smoke.cc
@@ -1,6 +1,9 @@
 #include "dawn/native/DawnNative.h"
+#include "dawn/native/OpenGLBackend.h"
 
 int main() {
+    static_assert(sizeof(dawn::native::opengl::GLuint) == sizeof(unsigned int));
+
     const DawnProcTable& procs = dawn::native::GetProcs();
     if (procs.createInstance == nullptr) {
         return 1;
